030d3e2fc32c3f63f9187cbce03621f6c4759915
gifa44 <katze.finchen@gmx.de>
2023-07-01 16:43:02 +0200

    Jahr hinzugefügt

--- getlyrics.py
+++ getlyrics.py
@@ -4,6 +4,9 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import lyricsgenius
 from tqdm import tqdm
 import json
+from datetime import datetime
+
+
 
 # Setzen Sie hier Ihre eigenen API-Schlüssel und Zugangsdaten ein
 SPOTIPY_CLIENT_ID = '46da6046625f409cb53ab06bf807861d'
@@ -29,6 +32,7 @@ songs_dict = {"Spotify": {}}
 # Durchlaufe jeden Track in der Playlist + ID festsetzen
 songs_wout_lyrics_0= 0
 id_0 = 0
+date_0 = 0
 year_0 = 0
 playlist = "Spotify"
 
@@ -37,13 +41,15 @@ for track_0 in tqdm(tracks['items']):
     # Holen Sie sich den Namen des Künstlers und den Titel des Tracks
     artist_name_0 = track_0['track']['artists'][0]['name']
     track_name_0 = track_0['track']['name']
-    year_name_0 = track_0['track']['album']['release_date']
+    date_name_0 = track_0['track']['album']['release_date']
+    year_name_0 = datetime.strptime(str(date_name_0),'%Y-%m-%d').strftime('%Y')
+
 
     # Suche nach dem Song auf Genius
     song_0 = genius.search_song(track_name_0, artist_name_0)
     if song_0:
         lyrics_0 = song_0.lyrics
-        songs_dict[playlist][id_0]= {"artist_name": artist_name_0, "track_name":track_name_0, "release_date":year_name_0, "lyrics": lyrics_0}
+        songs_dict[playlist][id_0]= {"artist_name": artist_name_0, "track_name":track_name_0, "release_date": date_name_0, "release_year": year_name_0,"lyrics": lyrics_0}
         id_0 += 1
     else:
         songs_wout_lyrics_0 += 1
