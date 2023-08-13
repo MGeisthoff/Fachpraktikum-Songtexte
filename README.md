078404ce2415e5c04626ab9a86cc067a16016ba3
gifa44 <katze.finchen@gmx.de>
2023-08-13 14:49:03 +0200

    Readme angepasst

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # Fachpraktikum-Songtexte
  
+ - Im File Packages.py sind alle Packages enthalten, die für dieses Projekt geladenen werden müssen, um das gesamte Projekt auszuführen. 
  
- 1. Spotify and LyricGenius API: 
- - Im Terminal ausführen: pip install spotipy and pip install lyricsgenius
+ 1. Spotify and LyricGenius API: (getlyrics.py)
+Um diese Datei ausführen zu können muss ein Konto bei Spotify sowie LyricsGenius erstellt werden. 
+Mit dieser Datei werden die Spotifysongs oder -playlisten ausgesucht und die entsprechenden Songtexte, Artists, Erscheinungsdatum sowie -jahre und die Popularität (eEigenwert bei Spotify) herausgefiltert. 
+Dann wird eine Datei all_songs_neu2.json erstellt, welche die zuvor genannten Daten enthält in einem Dictonary Spotify. Hier können auch mehrere Dictonaries für verschiedene Playlisten angelegt werden. 
+
+2. Analyse der Songs: (Analysis.py)
+Zur Unterstützung der Ausführung der Datei wurden drei CSV Dateien erstellt 
+- ausdruecke.csv (Quelle: https://de.wikipedia.org/wiki/Kategorie:Schimpfwort_(Person))
+- Mann_Frau.csv 
+- gender_neu_Nomen.csv (Quelle: https://geschicktgendern.de/ , Stand 06/2023)
+
+Nun wird auf die Datei aus 1. das Spacy Framework de_core_news_lg-3.5.0-py3-none-any.whl (Quelle: https://spacy.io/models/de/) ausgeführt. Dieses dient 
+
 
 
  NLP.py  -> Segmentation (aktuell nur zum Teilen von text in einzelne Textbausteine) -> kann wahrscheinlich durch cyspa ersetzt werden 
--- getlyrics.py
+++ getlyrics.py
@@ -1,27 +1,17 @@
-##Spotify and LyricsGenius API
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import lyricsgenius
-from tqdm import tqdm
-import json
-from datetime import datetime
-
-
-
 # Setzen Sie hier Ihre eigenen API-Schlüssel und Zugangsdaten ein
-SPOTIPY_CLIENT_ID = '46da6046625f409cb53ab06bf807861d'
-SPOTIPY_CLIENT_SECRET = '00efb0ab68db4f1eaf27ec53a4c1e068'
-GENIUS_ACCESS_TOKEN = 'OsBvEsJ_OLKYR42CJoaA_N97GNOS44XXFk4KPPcLtCYnWmDwnnzY2RpcG_SdG25a'
-# Erstelle eine Instanz von SpotifyClientCredentials mit Deinen Zugangsdaten
+SPOTIPY_CLIENT_ID = '' 
+SPOTIPY_CLIENT_SECRET = ''
+GENIUS_ACCESS_TOKEN = ''
+
 client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
 
-# Gib die ID der Playlist ein, aus der die Songtexte extrahiert werden sollen
-playlist_id ='75hKxWLF6z1rDdO5gFSCd2' # Playlist: Fachpraktikum Sprachentechnologie
+# Playlist-ID eingeben:
+playlist_id ='' # in diesem Projekt: 75hKxWLF6z1rDdO5gFSCd2 Playlist: Fachpraktikum Sprachentechnologie
 
-# die Spotify API lässt nur 100 Songs zu, daher die Schleife, um alle Songs zu bekommen
+# Da die Spotify API nur 100 Songs zulässt, kommt hier eine Schleife, um alle Songs zu bekommen
 offset=0
 tracks_alt = sp.playlist_tracks(playlist_id, offset= offset)
 all_tracks = []
@@ -36,7 +26,8 @@ while True:
     offset += len(tracks_neu['items'])
 
 
-
+# Instanz von Genius mit Zugangstoken
+genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN)
 
 #Dictionary anlegen, um eine Datei mit mehreren Playlisten zu erhalten
 songs_dict = {"Spotify": {}}
@@ -47,20 +38,19 @@ id_0 = 0
 date_0 = 0
 year_0 = 0
 playlist = "Spotify"
-# Erstelle eine Instanz von Genius mit Ihrem Zugangstoken
-genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN)
+
 
 for track_0 in tqdm(all_tracks):
 
-    # Holen Sie sich den Namen des Künstlers und den Titel des Tracks
+    # Holen Sie sich den Künstlernamen, Tracknamen, Erscheinungsdatum/Jahr, Popularität
     artist_name_0 = track_0['track']['artists'][0]['name']
     track_name_0 = track_0['track']['name']
     date_name_0 = track_0['track']['album']['release_date']
-    year_name_0 = int(date_name_0[:4]) # extract year
+    year_name_0 = int(date_name_0[:4]) 
     popularity_0 = track_0['track']['popularity']
 
 
-    # Suche nach dem Song auf Genius
+    # Suche Song auf Genius
     try:
         song_0 = genius.search_song(track_name_0, artist_name_0)
         if song_0:
